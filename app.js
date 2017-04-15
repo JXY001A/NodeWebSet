397dcfaa8fb0f062c289dd856004eacb3097b1ba
JXY001A <JXY001A@aliyun.com>
2017-04-15 14:52:08 +0800

    网站服务器启动程序及依赖组件

---
+++ app.js
@@ -0,0 +1,27 @@
+/**
+ * 
+ * @authors: JXY001A
+ * @Emali:   JXY001A@aliyun.com
+ * @date:    2017-04-15 14:09:24
+ * @desc:    (描述)
+ * @github:  github.com/JXY001A
+ * @version: 1.0
+ */
+var express = require('express');
+// var jade = require('jade');
+// 设置默认端口为3000，也可在启东时使用 PORT=4000 node app.js 来动态指定
+var port = process.env.PORT || 3000;
+var app = express();
+// 注意 app.set('views','./views') 中 views必须写对
+app.set('views', './views');
+app.set('view engine', 'jade');
+
+app.listen(port, function() {
+	console.log('程序运行在' + port + '端口');
+});
+
+app.get('/', function(req, res) {
+	res.render('index', {
+		title: 'movie index'
+	});
+});
