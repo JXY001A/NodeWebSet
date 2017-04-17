0ca61ab210538a8bb4e4000b1080a18a0fde158e
JXY001A <JXY001A@aliyun.com>
2017-04-17 14:24:31 +0800

    对整个项目结构进行了修改

--- app.js
+++ app.js
@@ -8,6 +8,9 @@
  * @version: 1.0
  */
 var express = require('express');
+var mongoose = require('mongoose');
+var url = 'mongodb://localhost:27017/imovie';
+
 // var jade = require('jade');
 // 设置默认端口为3000，也可在启东时使用 PORT=4000 node app.js 来动态指定
 var port = process.env.PORT || 3000;
@@ -23,5 +26,22 @@ app.listen(port, function() {
 app.get('/', function(req, res) {
 	res.render('index', {
 		title: 'movie index'
+	});
+});
+
+app.get('/movie/:id', function(req, res) {
+	res.render('detail', {
+		title: 'movie detail'
+	});
+});
+
+app.get('/admin/list', function(req, res) {
+	res.render('list', {
+		title: 'movie admin List'
+	});
+});
+app.get('/admin/movie', function(req, res) {
+	res.render('admin', {
+		title: 'movie admin '
 	});
 });
---
+++ schemas/movie.js
@@ -0,0 +1,49 @@
+var mongoose = require('mongoose');
+var MovieSchema = new mongoose.Schema({
+	doctor: String,
+	title: String,
+	language: String,
+	country: String,
+	summary: String,
+	flash: String,
+	poster: String,
+	year: Number,
+	meta: {
+		createAt: {
+			type: Date,
+			default: Date.now();
+		},
+		updateAt: {
+			type: Date,
+			default: Date.now();
+		}
+	}
+});
+// 该方法用于监听每一次数据的插入，对其进行处理
+MovieSchema.pre('save', function(next) {
+	if (this.isNew) {
+		this.meta.createAt = this.meta.updateAt = Date.now();
+	} else {
+		this.meta.updateAt = Date.now();
+	}
+	// 应该是继续执行下一条（递归实现）
+	next();
+});
+MovieSchema.statics = {
+	// 该方法用于取出当前数据库中的所有数据
+	fetch: function(cb) {
+		return this
+			.find({})
+			.sort('meta.updateAt')
+			.exec(cb);
+
+	},
+	findById: function(id, cb) {
+		return this
+			.findOne({
+				_id: id
+			})
+			.exec(cb);
+	}
+};
+moudle.export = MovieSchema;
