cecb2fb86ed94264bc3e99de99a232d0d65e0a97
JXY001A <JXY001A@aliyun.com>
2017-04-17 22:03:14 +0800

    修改错误

--- moudle/movie.js
+++ moudle/movie.js
@@ -1,4 +1,4 @@
 var mongoose = require('mongoose');
 var movieSchema = require('../schemas/movie.js');
 var Movie = mongoose.model('Movie', movieSchema);
-moudle.exports = Movie;
+module.exports = Movie;
